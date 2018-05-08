54814260c514bd3d8b0ac26a82195902e79966cd
Felippe Ladeira <felippe@movem.co.uk>
2018-05-08 14:17:58 +0100

    Removed magic __call method and added the Intercom Client methods instead

--- src/IntercomApi.php
+++ src/IntercomApi.php
@@ -2,32 +2,127 @@
 
 namespace Darkin1\Intercom;
 
+use Intercom\IntercomClient;
+
 /**
  * Class IntercomApi.
  */
 class IntercomApi
 {
     /**
-     * @var \Guzzle\Service\Client|IntercomBasicAuthClient
+     * @var \Intercom\IntercomClient
      */
     protected $intercom;
 
     /**
      * @param $intercom
      */
-    public function __construct($intercom)
+    public function __construct(IntercomClient $intercom)
     {
         $this->intercom = $intercom;
     }
 
     /**
-     * @param       $method
-     * @param array $args
-     *
-     * @return mixed
+     * @return \Intercom\IntercomUsers
      */
-    public function __call($method, array $args)
+    public function users()
     {
-        return $this->intercom->{$method};
+        return $this->intercom->users;
+    }
+
+    /**
+     * @return \Intercom\IntercomEvents
+     */
+    public function events()
+    {
+        return $this->intercom->events;
+    }
+
+    /**
+     * @return \Intercom\IntercomCompanies
+     */
+    public function companies()
+    {
+        return $this->intercom->companies;
+    }
+
+    /**
+     * @return \Intercom\IntercomMessages
+     */
+    public function messages()
+    {
+        return $this->intercom->messages;
+    }
+
+    /**
+     * @return \Intercom\IntercomConversations
+     */
+    public function conversations()
+    {
+        return $this->intercom->conversations;
+    }
+
+    /**
+     * @return \Intercom\IntercomLeads
+     */
+    public function leads()
+    {
+        return $this->intercom->leads;
+    }
+
+    /**
+     * @return \Intercom\IntercomAdmins
+     */
+    public function admins()
+    {
+        return $this->intercom->admins;
+    }
+
+    /**
+     * @return \Intercom\IntercomTags
+     */
+    public function tags()
+    {
+        return $this->intercom->tags;
+    }
+
+    /**
+     * @return \Intercom\IntercomSegments
+     */
+    public function segments()
+    {
+        return $this->intercom->segments;
+    }
+
+    /**
+     * @return \Intercom\IntercomCounts
+     */
+    public function counts()
+    {
+        return $this->intercom->counts;
+    }
+
+    /**
+     * @return \Intercom\IntercomBulk
+     */
+    public function bulk()
+    {
+        return $this->intercom->bulk;
+    }
+
+    /**
+     * @return \Intercom\IntercomNotes
+     */
+    public function notes()
+    {
+        return $this->intercom->notes;
+    }
+
+    /**
+     * @return \Intercom\IntercomVisitors
+     */
+    public function visitors()
+    {
+        return $this->intercom->visitors;
     }
 }
